b557f941b927471bd2c9830e6bd6a7643aa94ce0
Douvry <valentin.jabba@gmail.com>
2022-01-12 08:51:41 +0400

    Start login and register

---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <form>
+      <h3>Connexion</h3>
+
+      <div class="form-group">
+        <label>Adresse email: </label>
+        <input type="email" v-model="input.email" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label>Mot de passe: </label>
+        <input type="password" v-model="input.password" class="form-control" />
+      </div>
+
+      <button
+        type="button"
+        class="btn btn-dark btn-lg btn-block"
+        v-on:click="login()"
+      >
+        Se connecter
+      </button>
+
+      <p class="forgot-password text-right mt-2 mb-4">
+        <router-link to="/forgot-password">Mot de passe oublié ?</router-link>
+      </p>
+      <p class="forgot-password text-right mt-2 mb-4">
+        <router-link to="/register"
+          >Pas encore de compte ? Inscrivez-vous ici !</router-link
+        >
+      </p>
+    </form>
+  </div>
+</template>
+ 
+<script>
+import axios from "axios";
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    async login() {
+      if (this.input.email != "" && this.input.password != "") {
+        if (this.input.password.length >= 6) {
+          try {
+            console.log("OK");
+            /*let token = localStorage.getItem("token");
+            if (token) {*/
+                const res = await axios.post("http://localhost:3000/auth/login", {
+                email: this.input.email,
+                password: this.input.password,
+                },/*  {headers: {'Authorization': `Bearer ${token}`}} */);
+            console.log(res.data);
+            localStorage.token = res.data.access_token;
+            /*} */
+          } catch (error) {
+            console.error(error);
+          }
+        } else {
+          console.log("Mot de passe trop court");
+        }
+      } else {
+        console.log("A username and password must be present");
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container">
+        <form>
+            <h3>Inscription</h3>
+ 
+            <div class="form-group">
+                <label>Nom: </label>
+                <input type="text" v-model="input.lastname" class="form-control" />
+            </div>
+ 
+            <div class="form-group">
+                <label>Prenom: </label>
+                <input type="text" v-model="input.firstname" class="form-control" />
+            </div>
+
+            <div class="form-group">
+                <label>Adresse mail: </label>
+                <input type="email" v-model="input.email" class="form-control" />
+            </div>
+
+            <div class="form-group">
+                <label>Mot de passe: </label>
+                <input type="password" v-model="input.password" class="form-control" />
+            </div>
+
+            <div class="form-group">
+                <label>Confirmer mot de passe: </label>
+                <input type="password" v-model="input.passwordConfim" class="form-control" />
+            </div>
+
+            <div class="form-group">
+                <label>Numéro de chambre: </label>
+                <input type="text" v-model="input.roomNumber" class="form-control" />
+            </div>
+ 
+            <button type="button" class="btn btn-dark btn-lg btn-block" v-on:click="register()">S'inscrire</button>
+ 
+            <p class="text-right mt-2 mb-4">
+                <router-link to="/login">Vous possédez déjà un compte ? Connectez-vous ici !</router-link>
+            </p>
+        </form>
+    </div>
+</template>
+ 
+<script>
+    //import axios from 'axios';
+    export default {
+        name: 'Register',
+        data() {
+            return {
+                input: {
+                    lastname: "",
+                    firstname: "",
+                    email: "",
+                    password: "",
+                    passwordConfirm: "",
+                    roomNumber: ""
+                }
+            }
+        },
+        methods: {
+            async register() {
+            }
+        }
+    }
+</script>
